2d5b7fb6eeac5ed3ec256cc93eb7a32d522df9c4
adansio <adansio@gmail.com>
2012-11-11 00:26:16 -0300

    login-logout, modif datos, posts

--- app/app/models/user.rb
+++ app/app/models/user.rb
@@ -8,4 +8,6 @@ class User < ActiveRecord::Base
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me, :bio, :country, :birthday
   # attr_accessible :title, :body
+  #
+  has_many :posts
 end
